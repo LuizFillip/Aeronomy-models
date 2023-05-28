43dd2f9531f9d9b9629b5f5a63c0c84cd3fd70d2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-28 14:26:49 -0300

    convert into dictonary point_msis functions

--- src/msis.py
+++ src/msis.py
@@ -25,15 +25,16 @@ def point_msis(dn, zeq, glat, glon):
     """
     
     t = get_indices(dn.date())
-     
+    
     res = msise_flat(
-        dn, zeq, glat, glon, 
-        t.get("F10.7a"), t.get("F10.7obs"), t.get("Ap")
-        )
-    
-    He, O, N2, O2, Ar, mass, H, N, AnO, Tex, Tn = tuple(res)
-    
-    return He, O, N2, O2, H, N, Tn
+       dn, zeq, glat, glon, 
+       t.get("F10.7a"), t.get("F10.7obs"), t.get("Ap")
+       )
+
+    names = ["He", "O", "N2", "O2", "Ar", "mass", 
+            "H", "N", "AnO", "Tex", "Tn"]
+
+    return {key: value for key, value in zip(names, res)}
 
 
 def altrange_msis(
@@ -95,54 +96,6 @@ def main():
     site = "saa"
     glat, glon = sites[site]["coords"]
     dn = dt.datetime(2013, 1, 1)
-    
-    df =  altrange_msis(
-            dn,
-            glat, 
-            glon,
-            hmin = 200, 
-            hmax = 500, 
-            step = 1, 
-            )
-    
-    df
-    
-    
-def con_from_point(
-        tn, 
-        o_point, 
-        o2_point, 
-        n2_point, 
-        step,  
-        base_height = 200.0 
-        ):
-    
-    """
-    Get concetrantions profiles from neutral constituints 
-    from point
-    """
-
-    CO = np.zeros(len(tn))
-    CO2 = np.zeros(len(tn))                
-    CN2 = np.zeros(len(tn))                
-
-    
-    for i in range(0, len(tn)):
+    zeq = 300
         
-        Z1 = base_height + step * i
-        
-        GR = 1.0 / pow(1.0 + Z1 / 6370.0, 2)
-        
-        HO = 0.0528 * tn[i] / GR                               # scale height of O [km]
-        HO2 = 0.0264 * tn[i] / GR                              # scale height of O2 [km]
-        HN2 = 0.0302 * tn[i] / GR                              # scale height of N2 [km]
-
-        p_co = o_point / 5.33 * 8.55
-        p_co2 = o2_point / 1.67 * 4.44
-        p_cn2 = n2_point / 9.67 * 2.26
-
-        CO[i] = p_co * np.exp(-(Z1 - 335.0) / HO)           # atomic oxygen [cm-3]
-        CO2[i] = p_co2 * np.exp(-(Z1 - 335.0) / HO2)        # molecular oxygen [cm-3]
-        CN2[i] = p_cn2 * np.exp(-(Z1 - 335.0) / HN2) 
-    
-    return CO, CO2, CN2
+    point_msis(dn, zeq, glat, glon)
