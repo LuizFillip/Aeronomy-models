9b88311bea4c91a9531d086ee663c257c62bb33b
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-29 09:59:52 -0300

    update init

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,7 @@
-from .iri import point_iri, altrange_iri, ne_from_latitude
+from .iri import (point_iri, 
+                  altrange_iri, 
+                  ne_from_latitude, 
+                  timeseries_iri)
 from .msis import altrange_msis, timerange_msis, point_msis
 from .core import altrange_models, point_models
 from .hwm import load_hwm
