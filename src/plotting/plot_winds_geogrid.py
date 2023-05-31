7d30c2f69eeb561cb748ac312cd4da46ed20bc08
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-05-31 14:35:31 -0300

    plotting grid winds

--- src/plotting/plot_winds_geogrid.py
+++ src/plotting/plot_winds_geogrid.py
@@ -1,127 +1,154 @@
 import pandas as pd
-from GEO import map_attrs
+import GEO as g
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import settings as s
 import numpy as np
-from utils import save_but_not_show
 import matplotlib as mpl
+import datetime as dt
 
 
-
-
-
-
-
-def save_plots(ds, coord = "meridional"):
-    """
-    Running lastly functions and save plots
-    """
-    for dn in pd.to_datetime(ds.time.values):
-        
-        fig = plot_winds_over_map(ds, dn, coord = coord)
-        
-        FigureName = dn.strftime("%Y%m%d%H%M") + ".png"
-        save_in = f"D:\\plots\\HWM14\\2013\\{coord}\\{FigureName}"
-        
-        print("save...", dn)
-        
-        save_but_not_show(fig, save_in)
-
-# save_plots(ds, coord = "zonal")
-
-
-
-
-def plot_grid_wind(
-        ax, dn, values, 
-        lons, lats, vmin, vmax):
+def plot_grid(
+        ax, 
+        ds, 
+        time, 
+        coord = "mer",
+        cmap = "rainbow"):
     
-
-    levels = np.linspace(vmin, vmax, 30)
+    ds = ds.loc[ds["dn"] == time]
+  
+    df = pd.pivot_table(
+        ds, 
+        values = coord, 
+        index = "lat", 
+        columns = "lon"
+        )
     
-    img = ax.contourf(lons, lats, values, 
-                      levels = levels,
-                      cmap = "jet")
-    
-    lat_lims = dict(min = -10, 
-                    max = -2,
-                    stp = 1)
-
-    lon_lims = dict(min = -45, 
-                    max = -35, 
-                    stp = 1)    
-    
-    map_attrs(ax, lon_lims, lat_lims)
-    
-    ax.set(title = dn)
-    
-    return ax, img
-
-
-infile = "database/HWM/grid_winds_20130101.txt"
-
-def set_data(infile, coord = "mer"):
-    
-    ds = pd.read_csv(infile, index_col = 0)
-    
-    
-    ds = ds.loc[(ds["lat"] <= -2) &
-                (ds["lat"] >= -10) &
-                (ds["lon"] >= -45) & 
-                (ds["lon"] <= -35)]
-
     vmin = round(ds[coord].min())
     vmax = round(ds[coord].max())
-    return ds, vmin, vmax
-
-def plot_winds_over_map(infile, coord = "zon"):
-
-    fig, ax = plt.subplots( 
-        figsize = (12, 12),
-        dpi = 300, 
-        ncols = 2,
-        nrows = 2,
-        sharex= True, sharey=True, 
-        subplot_kw = {'projection': ccrs.PlateCarree()}
+    
+    levels = np.linspace(vmin, vmax, 30)
+    
+    ax.contourf(
+        df.columns,
+        df.index, 
+        df.values, 
+        levels = levels,
+        cmap = cmap
         )
-    plt.subplots_adjust(hspace = 0., wspace = 0.2)
     
     
+    ax.set(title = time.strftime("%d/%m %H:%M"))
     
-    ds, vmin, vmax =  set_data(infile, coord = coord)
+    return ax
     
-    times = ds["time"].unique()
     
-    for num, ax in enumerate(ax.flat):
-                
-        ds1 = ds.loc[(ds["time"] == times[num])]
-        
-        df = pd.pivot_table(
-            ds1, 
-            values = coord, 
-            index = "lat", 
-            columns = "lon"
-            )
-        
-        plot_grid_wind(
-                ax, times[num], df.values, 
-                df.columns, df.index, 
-                vmin, vmax
-                )
-        
-        
-    norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
-    cax = plt.axes([0.95, 0.15, 0.03, 0.68])
+def plot_colorbar(
+        fig,
+        vmin, 
+        vmax, 
+        coord = "mer", 
+        rainbow = "rainbow",
+        fontsize = 40
+        ):
+    
+    norm = mpl.colors.Normalize(
+        vmin = vmin, vmax=vmax
+        )
+   
+    cax = plt.axes([0.2, 1.001, 0.6, 0.02])
     
     if coord == "zon":
         label = "Velocidade zonal (m/s)"
     else:
         label = "Velocidade meridional (m/s)"
-    fig.colorbar(
-        mpl.cm.ScalarMappable(norm = norm, cmap = "rainbow"),
-             label = label, cax = cax)
+        
+    cb = fig.colorbar(
+        mpl.cm.ScalarMappable(
+            norm = norm, 
+            cmap = rainbow
+            ),
+        ticks = np.arange(vmin, vmax, 10),
+        label = label, 
+        cax = cax, 
+        orientation = "horizontal", 
+        )
+    cb.set_label(label, fontsize = fontsize)
+
+infile = "database/HWM/grid_2013_03.txt"
+ds = pd.read_csv(infile)
+
+ds["dn"] = pd.to_datetime(ds["dn"])
+
+def plot_winds_geogrid(ds, coord = "mer", 
+                       fontsize = 40):
+    fig, ax = plt.subplots( 
+          figsize = (20, 15),
+          dpi = 300, 
+          ncols = 5, 
+          nrows = 3,
+          sharex= True, 
+          sharey=True, 
+          subplot_kw = {'projection': ccrs.PlateCarree()}
+          )
+    
+    
+    plt.subplots_adjust(hspace = 0.2, wspace = 0.001)
+    
+    
+    vmin = round(ds[coord].min())
+    vmax = round(ds[coord].max())
+    
+    
+    vlats = dict(min = -30, max = 10, stp = 5)
+    vlons = dict(min = -60, max = -30, stp = 5)   
+    
+    delta = dt.timedelta(hours = 4)
+    
+    for row, day in enumerate([16, 17, 18]):
+        
+        dn = dt.datetime(2013, 3, day, 22, 0)
+        times = pd.date_range(dn, dn + delta, freq = "1H")
+        
+        for col, time in enumerate(times):
+            
+            plot_grid(
+                ax[row, col], 
+                ds, 
+                time
+                )
+            g.mag_equator(ax[row, col])
+            g.map_features(ax[row, col])
+    
+            
+            ax[row, col].set(
+                ylim = [vlats['min'], vlats['max']], 
+                xlim = [vlons['min'], vlons['max']]
+                )
+            
+        ax[row, 0].set(
+            xticks = np.arange(
+                vlons['min'], vlons['max'] + 10, 10
+                ), 
+            yticks = np.arange(
+                vlats['min'], vlats['max'] + 10, 10
+                )
+            )
+            
+    s.config_labels(fontsize = 20)
+    plot_colorbar(fig, vmin, vmax, coord = coord)
+    
+    
+    fig.text(.07, 0.43, "Latitude (°)", 
+             rotation = "vertical", fontsize = fontsize)
+    fig.text(.43, 0.07, "Longitude (°)", 
+             fontsize = fontsize)
+    
+    plt.show()
     
     return fig
 
-fig = plot_winds_over_map(infile, coord = "zon")
+f= plot_winds_geogrid(ds, coord = "mer", 
+                       fontsize = 40)
+
+f.savefig("models/figures/windsgrid.png", dpi = 300)
