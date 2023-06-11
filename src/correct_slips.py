4d6a8700b6ff54e4b0250bcbd78bbe20d2fd2985
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2023-06-11 18:29:38 -0300

    rewrinting the function

--- src/correct_slips.py
+++ src/correct_slips.py
@@ -1,66 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import FabryPerot as fp
-import models as m
 import pandas as pd
-import datetime as dt
-from common import load
+from utils import smooth
+import settings as s
 
 
-def demonstrated():
-    # Example phase measurements with cycle slips
-    phases = np.array([1.0, 2.0, -3.0, -2.0, 4.0, 5.0, -6.0, -5.0])
+
+
+def find_slip(vls, threshold = 8):
+    diff = np.diff(vls)
+    index = [i for i, v in enumerate(diff) 
+             if diff[i] > threshold][0]
+    cycle = vls[index] - vls[index + 1]
+    vls[index + 1:]  += cycle
+    return vls
+
+
+def correct_and_smooth(
+        vls, 
+        threshold = 8, 
+        pts = 10, 
+        window = 2
+        ):
+    vls = find_slip(vls, threshold = threshold)
+    return smooth(vls, pts = pts, window = 2)
+
+def plot_result():
+    df = pd.read_csv('test_temp.txt', index_col=0)
+    df.index = pd.to_datetime(df.index)
     
-    # Correct cycle slips
-    corrected_phases = correct_cycle_slips(phases)
     
-    plt.plot(phases)
-    plt.plot(corrected_phases)
+    vls = df['Tn'].values
     
-
-path = 'database/FabryPerot/2012/minime01_car_20130318.cedar.005.txt'
-tp = fp.FPI(path).temp
-
-def correct_cycle_slips(unwrapped_phases):
-    # unwrapped_phases = np.unwrap(phases)
-    diff_phases = np.diff(unwrapped_phases)
-    cycle_slip_indices = np.where(np.abs(diff_phases) > np.pi)[0] + 1
-
-    for index in cycle_slip_indices:
-        diff = unwrapped_phases[index] + unwrapped_phases[index - 1]
-        cycles = np.round(diff / (2 * np.pi))
-        unwrapped_phases[index:] += cycles * 2 * np.pi
-
-    return unwrapped_phases
-
-
-# 
-
-fig, ax = plt.subplots()
-
-
-df = pd.read_csv('test_temp.txt', index_col=0)
-df.index = pd.to_datetime(df.index)
-
-# 
-
-
-
-vls = df['Tn'].values
-
-diff = np.diff(vls)
-
-threshold = 10
-for i, v in enumerate(diff):
-    if diff[i] > threshold:
-        cycle = (vls[i] - vls[i + 1])
-        index = i
-print
-df['cTn'] = vls
-
-
-ax.plot(df['cTn'])
-ax.plot(df['Tn'])
-
-
-len(vls), len(diff)
+    fig, ax = plt.subplots(dpi = 300)
+    
+    
+    ax.plot(df.index, vls, label = 'original')
+    vls = find_slip(vls, threshold = 8)
+    
+    ax.plot(df.index, vls, label = 'corrigido')
+    ax.plot(df.index, smooth(vls, pts = 10, window = 2), 
+            label = 'corrigido e suavizado')
+    
+    ax.legend()
+    
+    ax.set(ylabel = 'Temperatura (K)', 
+           xlabel = 'Hora universal')
+    
+    s.format_time_axes(ax, hour_locator = 1)
+    
+# plot_result()
